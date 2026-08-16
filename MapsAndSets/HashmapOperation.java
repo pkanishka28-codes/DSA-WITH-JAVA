730733609b8beb49bfca673baebaaa2e32c45a58
kanishka <pandeykanishka285@gmail.com>
2026-08-16 18:53:15 +0530

    updated file

--- MapsAndSets/HashmapOperation.java
+++ MapsAndSets/HashmapOperation.java
@@ -7,12 +7,16 @@ public class HashmapOperation {
         h1.put("niya",89);
         h1.put("nick",97);
         h1.put("max",98);
+        h1.put("nitya", 92);
           
         System.out.println("Size is : "+h1.size());
         //Get
         System.out.println(h1.get("ravi"));
+
         //ContainsKey
         System.out.println(h1.containsKey(97));
+        System.out.println(h1.containsKey(91));
+
         //Remove
         h1.remove("niya");
 
